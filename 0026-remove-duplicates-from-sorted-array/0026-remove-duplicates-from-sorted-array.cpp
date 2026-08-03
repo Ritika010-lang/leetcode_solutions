799a47fe9798f9fde3fcb332ba752880c36c1ee2
Ritika Goel <goelritika2005@gmail.com>
2026-08-03 18:39:03 +0530

    Time: 0 ms (100%), Space: 22.8 MB (19.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,8 +4,8 @@ public:
         int i = 0;
         for(int j = 1; j < arr.size(); j++){
             if(arr[j] != arr[i]){
+                arr[i + 1] = arr[j];
                 i++;
-                arr[i] = arr[j];
             }
             
         }
